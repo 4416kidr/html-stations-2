aecc70614d7656246db93e1cd756bb84f9bff087
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2023-03-12 03:22:41 +0900

    clear station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,11 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const flag = document.getElementById("check").checked;
+  const text = document.getElementById("text");
+  if (flag) {
+    text.style.backgroundColor = "red";
+  }
+  else {
+    text.style.backgroundColor = "white"
+  }
 }
